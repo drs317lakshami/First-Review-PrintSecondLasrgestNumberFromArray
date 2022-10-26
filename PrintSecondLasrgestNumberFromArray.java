1c3be933af3edea79ff16874f7fe18877eafdf87
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-10-26 09:11:18 +0530

    Print Second Largest Number From Array

--- PrintSecondLasrgestNumberFromArray.java
+++ PrintSecondLasrgestNumberFromArray.java
@@ -1,37 +1,17 @@
 package com.blz.firstday;
-	//created class
-public class PrintSecondLasrgestNumberFromArray {
-	
-	//created function to print second largest elements
-	static void print2largest(int arr[],int arr_size) {
-		
-		int i, first,second;
-		
-		//there should be atleast two element to check
-			if(arr_size<2) {
-				System.out.println("invalid input");
-			return ;
-			
-		}
-			//sort the array
-		Arrays.sort(arr);
-		
-		//start from second largest element 
-		for(i = arr_size-2; i>+0; i--) {
-			
-			if (arr[i]!=arr[arr_size-1]) {
-				
-				System.out.println("the second largest no is "  +arr[i]);
-				return ;
-			}
-			}
-	}
-	
- public static void main(String[] args) {
-	 int arr[] = {2,3,4,5,2};
-	 
-	 int n=arr.length;
-	 print2largest(arr,n);
-	
-}
+	public class PrintSecondLasrgestNumberFromArray {
+    public static void main(String[] args) {
+        int[] a = {20,30,50,60};
+        int temp;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] < a[j]) {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        System.out.println("minimum element from array: " + a[1]);
+    }
 }
